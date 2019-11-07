df615512d18014cc209b0ef0f6ff5f30f30ae994
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-11-06 20:19:52 -0500

    g

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HalfPixelsSite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalWebsite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
